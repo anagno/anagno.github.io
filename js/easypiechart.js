98d9d8d97aa212c58f987e3be26945b2d0fc6971
anagno <anagnwstopoulos@hotmail.com>
2019-04-20 23:10:48 +0200

    Fininshing the first edition

---
+++ js/easypiechart.js
@@ -0,0 +1,11 @@
+$(function () {
+	$('.chart').easyPieChart({
+		scaleColor: false,
+		lineWidth: 6,
+		lineCap: 'round',
+		barColor: '#ff7657',
+		trackColor: "#f2f2f2",
+		size: 135,
+		animate: 1000
+	});
+});
